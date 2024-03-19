c26a1071c4f37241182487cfeaec53fe25d57889
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-03-19 10:29:17 +0300

    add array

---
+++ Array.java
@@ -0,0 +1,8 @@
+public class Array {
+    public static void main(String[] args) {
+        String[] cars = { "Volvo", "Mazda", "BMW", "Mercedes Benz" };
+        for (String i : cars) {
+            System.out.println(i);
+        }
+    }
+}
