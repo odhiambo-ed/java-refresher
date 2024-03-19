19ec1d5481bd234f9839e6697786e02f6ca59a3f
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-03-19 11:35:35 +0300

    add class 2

---
+++ MyClass2.java
@@ -0,0 +1,10 @@
+public class MyClass2 {
+    public void myMethod() {
+        System.out.println("Hello to you?");
+    }
+
+    public static void main(String[] args) {
+        MyClass2 myObj = new MyClass2();
+        myObj.myMethod();
+    }
+}
